ecfbbb836d6240a2f5c13b18cca9815e0703b8f8
rumblingturtle <edzaljaev@gmail.com>
2019-04-26 00:29:17 +0300

    Final, For conditions and Variadic templates tests

---
+++ tests/Final - Eduard Zalyaev/001.cpp
@@ -0,0 +1,21 @@
+/**
+ * Test covers basic final specifier for a virtual function 
+ * 
+ * Done by Eduard Zalyaev
+*/
+
+struct A
+{
+	virtual int foo() {return -1;}
+};
+
+struct B : A
+{
+	int foo() final { return 42;}
+};
+
+int main()
+{
+	B b = B();
+	return !(b.foo() == 42);
+}
---
+++ tests/Variadic templates - Eduard Zalyaev/004e.cpp
@@ -0,0 +1,34 @@
+/**
+ * Test nested template packs
+ *
+ * "A parameter pack whose name appears within the pattern of a pack expansion is expanded
+ * by that packexpansion. An appearance of the name of a parameter pack is only expanded by 
+ * the innermost enclosingpack expansion. The pattern of a pack expansion shall name one or more parameter
+ * packs that are notexpanded by a nested pack expansion. All of the parameter packs expanded by a pack expansion 
+ * shall havethe same number of arguments specified"
+ *
+ * Done by Eduard Zalyaev
+*/
+#include <iostream>
+template<typename ...args1>
+struct A1 {
+	template<typename ...args2 >
+	struct A2
+	{
+		static void printer(args1 ...a1, args2 ...a2)
+		{
+			((std::cout << a1 << " "), ...);
+			((std::cout << a2 << " "), ...);
+		}
+	};
+
+};
+
+
+int main()
+{
+	A1<char*, char*, char*, char*>::A2<char*, char*, char*, char*>::printer("I", "A", "M", "B", "O", "R", "E", "D");
+	A1<char*>::A2<char*, char*>::printer("W", "O", "W");
+	
+	return 0;
+}
